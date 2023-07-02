92acd6b075d2546f8c75f5db2631fce59c73e111
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-07-01 22:22:50 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,80 +1,78 @@
 shell.run("clear")
 print("Welcome to KVERO OS!")
-location = "" -- to be used l8r
+location = "" -- to be used later
 inmain = 0
 
-if installer then
-	installer = 0
-else
-	installer = 1
+local configFile = "config.txt"
+
+-- Read the installer state from the configuration file
+local installer = "1" -- Default value if the file doesn't exist
+if fs.exists(configFile) then
+    
+    local file = io.open(configFile, "r")
+    installer = file:read()
+    file:close()
+    
 end
 
-local success, result = pcall(function()
-	shell.run("installer.lua")
-end)
-
-
-if not success then
-	installer = 1
-end
-
-if installer == 1 then
-	print("Try or install KVERO t/i")
-	while true do
-		res = io.read()
-		if res == "i" or res == "t" then
-			break
-		end
-	end
-	if res == "i" then
-		file = io.open("installer.lua", "w")
-		file:write("installer = 0")
-		file:close()
-		installer = 0
-		io.open("startup.lua")
-		shell.run("cp /disk/* /startup.lua")
-	end
+if installer == "1" then
+    print("Try or install KVERO t/i")
+    while true do
+        res = io.read()
+        if res == "i" or res == "t" then
+            break
+        end
+    end
+    if res == "i" then
+        -- Update the installer state in the configuration file
+        local file = io.open(configFile, "w")
+        file:write("0")
+        file:close()
+        
+        -- Perform installation tasks
+        io.open("startup.lua")
+        shell.run("cp /disk/* /startup.lua")
+    end
 end
 
 while true do
-	io.write(location .. " > ")
-	user = io.read()
-	if inmain == 0 then
-		if user == "mainshell" then
-			inmain = 1
-		else
-			if user == "driver mod" then
-				print("KVERO driver mod")
-				if drivers then
-					io.write("drivers exist, create new driver? y/n: ")
-					res = io.read()
-					if res == "yes" or res == "y" then
-						io.write("driver name: ")
-						res = io.read()
-						io.write("driver file path: ")
-						res1 = io.read()
-						drivers[res] = res1
-					end
-				else
-					io.write("create new driver system? y/n: ")
-					res = io.read()
-					if res == "yes" or res == "y" then
-						io.write("driver name: ")
-						res = io.read()
-						io.write("driver file path: ")
-						res1 = io.read()
-						drivers = {}
-						drivers[res] = res1
-					end
-				
-				end
-			end
-		end
-	else
-		if user == "exit mainshell" then
-			inmain = 0
-		else
-			shell.run(user)
-		end
-	end
+    io.write(location .. " > ")
+    user = io.read()
+    if inmain == 0 then
+        if user == "mainshell" then
+            inmain = 1
+        else
+            if user == "driver mod" then
+                print("KVERO driver mod")
+                if drivers then
+                    io.write("drivers exist, create new driver? y/n: ")
+                    res = io.read()
+                    if res == "yes" or res == "y" then
+                        io.write("driver name: ")
+                        res = io.read()
+                        io.write("driver file path: ")
+                        res1 = io.read()
+                        drivers[res] = res1
+                    end
+                else
+                    io.write("create new driver system? y/n: ")
+                    res = io.read()
+                    if res == "yes" or res == "y" then
+                        io.write("driver name: ")
+                        res = io.read()
+                        io.write("driver file path: ")
+                        res1 = io.read()
+                        drivers = {}
+                        drivers[res] = res1
+                    end
+                end
+            end
+        end
+    else
+        if user == "exit mainshell" then
+            inmain = 0
+        else
+            shell.run(user)
+        end
+    end
 end
