621b727b46bb7f65600f9da2c10935e7a4b05007
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-07-01 22:15:08 -0500

    Create beta2.lua

---
+++ beta2.lua
@@ -0,0 +1,73 @@
+shell.run("clear")
+print("Welcome to KVERO OS!")
+location = "" -- to be used later
+inmain = 0
+
+local configFile = "config.txt"
+
+-- Read the installer state from the configuration file
+local file = io.open(configFile, "r")
+local installer = file and file:read() or "1"
+file:close()
+
+if installer == "1" then
+    print("Try or install KVERO t/i")
+    while true do
+        res = io.read()
+        if res == "i" or res == "t" then
+            break
+        end
+    end
+    if res == "i" then
+        -- Update the installer state in the configuration file
+        file = io.open(configFile, "w")
+        file:write("0")
+        file:close()
+        
+        -- Perform installation tasks
+        io.open("startup.lua")
+        shell.run("cp /disk/* /startup.lua")
+    end
+end
+
+while true do
+    io.write(location .. " > ")
+    user = io.read()
+    if inmain == 0 then
+        if user == "mainshell" then
+            inmain = 1
+        else
+            if user == "driver mod" then
+                print("KVERO driver mod")
+                if drivers then
+                    io.write("drivers exist, create new driver? y/n: ")
+                    res = io.read()
+                    if res == "yes" or res == "y" then
+                        io.write("driver name: ")
+                        res = io.read()
+                        io.write("driver file path: ")
+                        res1 = io.read()
+                        drivers[res] = res1
+                    end
+                else
+                    io.write("create new driver system? y/n: ")
+                    res = io.read()
+                    if res == "yes" or res == "y" then
+                        io.write("driver name: ")
+                        res = io.read()
+                        io.write("driver file path: ")
+                        res1 = io.read()
+                        drivers = {}
+                        drivers[res] = res1
+                    end
+                end
+            end
+        end
+    else
+        if user == "exit mainshell" then
+            inmain = 0
+        else
+            shell.run(user)
+        end
+    end
+end
